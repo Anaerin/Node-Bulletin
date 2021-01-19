fb7936abcda6a9f34480237702977f0e56490016
Robert Johnston <anaerin@gmail.com>
2021-01-18 19:14:42 -0600

    Now with broadcasting and multicast, just in case

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 `use strict`
-import dgram from "dgram";
+import Broadcast from "./lib/broadcast.js";
+import Multicast from "./lib/multicast.js";
 import process from "process";
 
-const PORT = 20000;
-const MULTICAST_ADDR = "233.255.255.255";
-const socket = dgram.createSocket({
-	type: "udp4",
-	reuseAddr: true
-});
-socket.bind(PORT);
-socket.on("listening", () => {
-	socket.addMembership(MULTICAST_ADDR);
-	socket.setMulticastLoopback(true);
-	const address = socket.address;
-	console.log(`UDP socket listening on ${address.address}:${address.port}, PID:${process.pid}`);
-	setInterval(sendMessage, 2500);
-});
+let multicast = new Multicast(printMulticastMessage);
+let broadcast = new Broadcast(printBroadcastMessage);
 
-socket.on("message", (message, rinfo) => {
-	console.info(`Message from: ${rinfo.address}:${rinfo.port} - ${message}`);
-});
-
-function sendMessage() {
-	const message = Buffer.from(`Message from process ${process.pid}`);
-	socket.send(message, 0, message.length, PORT, MULTICAST_ADDR, () => {
-		console.log(`Sending message: ${message}`);
-	});
+function printMulticastMessage(message) {
+	console.info(`Got Multicast message: ${message}`);
 }
+function printBroadcastMessage(message) {
+	console.info(`Got Broadcast message: ${message}`);
+}
+
+function sendMessages() {
+	multicast.send(`Hello from ${process.pid}`);
+	broadcast.send(`Hello from ${process.pid}`);
+}
+
+setInterval(sendMessages, 2500);
